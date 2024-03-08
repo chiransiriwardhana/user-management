d017023cb437cd4d92e96e64d4b37ae943eebf87
Chiran-Siriwardana <47823522+chiransiriwardhana@users.noreply.github.com>
2024-03-08 12:53:52 +0530

    Update usr-edit.component.ts

--- src/app/usr-edit/usr-edit.component.ts
+++ src/app/usr-edit/usr-edit.component.ts
@@ -23,6 +23,7 @@
   }>;
 
   constructor(private fb: NonNullableFormBuilder, private _usrService: UserService) {
+    // validate user inputs of edit pop up window.
     this.usrForm = this.fb.group({
       id:[0, [Validators.required]],
       firstName: ['', [Validators.required]],
@@ -33,22 +34,25 @@
     this.ID = 1
   }
   showModal(): void {
+    // when user click edit button, pop up window become visible
     this.isVisible = true;
     console.log("editing...")
   }
 
   handleCancel(): void {
+      // when user click cancel button, pop up window become invisible
       this.isVisible = false;
   }
 
   submitForm(id: number): void {
     if (this.usrForm.valid) {
         this.usrForm.value.id = id
+        // when user click submit button of edit pop up window, user data is updated
         this._usrService.updateUser(id, this.usrForm.value).subscribe({
           next: (val: any) => {
               
             this.isConfirmLoading = true;
-
+            // After 1 second pop window is closed
             setTimeout(() => {
               this.isVisible = false;
               this.isConfirmLoading = false;
@@ -57,6 +61,7 @@
             console.log('response from put: ', val);
               
           },
+            // If there is error when updating user details, prints it into console.
             error:(err: any) => {
               console.log("error", err)           
           }
