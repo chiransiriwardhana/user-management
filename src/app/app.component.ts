54f3a11367897bc94fa71ef6f91be44aa791dc6e
Chiran-Siriwardana <47823522+chiransiriwardhana@users.noreply.github.com>
2024-03-08 12:57:08 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@
   })
   export class AppComponent implements OnInit {
     title = 'user-management-system';
-
+    // When application is loading this function is triggered to load data from the server. Then data is shown in table view
     ngOnInit(): void {
       this.getUserList()
     }
@@ -17,7 +17,7 @@
     constructor(private _usrService: UserService) {}
 
     public listOfData: User[] = []
-    
+    // When application is loading this function is triggered to load data from the server. Then data is shown in table view
     getUserList() {
       this._usrService.getUser().subscribe({
         next: (res) => {
