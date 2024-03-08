544751c8468bb4cfb9d329cb5d05ce886573943b
Chiran-Siriwardana <47823522+chiransiriwardhana@users.noreply.github.com>
2024-03-08 12:43:22 +0530

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,19 +8,19 @@ import { Observable, catchError } from 'rxjs';
 export class UserService {
 
   constructor(private _http: HttpClient) { }
-
+  // Add new user
   addUser(data: any): Observable<any> {
     return this._http.post('https://dummyjson.com/users/add', data)    
   }
-
+  // fetch data from endpoint
   getUser(): Observable<any> {
     return this._http.get('https://dummyjson.com/users')   
   }
-
+  // delete user
   deleteUser(id: number): Observable<any> {
     return this._http.delete(`https://dummyjson.com/users/${id}`)   
   }
-
+  // update user details
   updateUser(id: number, data: any): Observable<any> {
     return this._http.put(`https://dummyjson.com/users/${id}`, data)
   }
